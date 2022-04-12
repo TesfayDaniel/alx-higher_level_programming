a2ce46bb5854290dad70e6c2fe6e566be468b06a
TesfayDaniel <tesfaydaniel11@gmail.com>
2022-04-12 04:40:59 -0700

    How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,25 @@
 #!/usr/bin/node
 
 const request = require('request');
-request.get(process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  let obj = {}
-  let data = JSON.parse(body)
-  for (let i = 0; i < data.length; i++)
-  {
-    if (data[i].completed == true)
-    {
-       obj[data[i].userId] = data[i].id;
+
+request(process.argv[2], function (err, _res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const completedTasksByUsers = {};
+    body = JSON.parse(body);
+
+    for (let i = 0; i < body.length; ++i) {
+      const userId = body[i].userId;
+      const completed = body[i].completed;
+
+      if (completed && !completedTasksByUsers[userId]) {
+        completedTasksByUsers[userId] = 0;
+      }
+
+      if (completed) ++completedTasksByUsers[userId];
     }
+
+    console.log(completedTasksByUsers);
   }
-  console.log(obj);
 });
