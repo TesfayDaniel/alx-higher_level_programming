92eda285bfbf4ac66f120386dd899e24a68ee2bb
TesfayDaniel <tesfaydaniel11@gmail.com>
2022-04-12 04:38:26 -0700

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,24 @@
 #!/usr/bin/node
 
 const request = require('request');
-request.get('https://swapi-api.hbtn.io/api/people/18', function (err, res, body) {
-  if (err) throw err;
-  console.log(JSON.parse(body).films.length);
+const starWarsUri = process.argv[2];
+let times = 0;
+
+request(starWarsUri, function (_err, _res, body) {
+  body = JSON.parse(body).results;
+
+  for (let i = 0; i < body.length; ++i) {
+    const characters = body[i].characters;
+
+    for (let j = 0; j < characters.length; ++j) {
+      const character = characters[j];
+      const characterId = character.split('/')[5];
+
+      if (characterId === '18') {
+        times += 1;
+      }
+    }
+  }
+
+  console.log(times);
 });
